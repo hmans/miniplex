f8ff3c20880771daffe107467878320ddd309664
Hendrik Mans <hendrik@mans.de>
2022-02-10 19:41:01 +0100

    Fix type issues in react.ts

--- src/react.tsx
+++ src/react.tsx
@@ -1,12 +1,11 @@
-import { createContext, useState, useContext, useEffect } from "react"
-import { ComponentName, createECS } from "./ecs"
-import { IEntity, ECS } from "./ecs"
+import { createContext, FC, useContext, useEffect, useState } from "react"
+import { ComponentName, createECS, ECS, IEntity } from "./ecs"
 import { useRerender } from "./util/useRerender"
 
 export function makeECS<T extends IEntity>() {
-  const context = createContext<ECS<T>>(null)
+  const context = createContext<ECS<T>>(null!)
 
-  function ECSProvider({ children }) {
+  const ECSProvider: FC = ({ children }) => {
     const [ecs] = useState(() => createECS<T>())
     return <context.Provider value={ecs}>{children}</context.Provider>
   }
@@ -21,8 +20,8 @@ export function makeECS<T extends IEntity>() {
     const archetype = ecs.archetype(...names)
 
     useEffect(() => {
-      ecs.listeners.archetypeChanged.get(archetype).on(rerender)
-      return () => ecs.listeners.archetypeChanged.get(archetype).off(rerender)
+      ecs.listeners.archetypeChanged.get(archetype)!.on(rerender)
+      return () => ecs.listeners.archetypeChanged.get(archetype)!.off(rerender)
     }, [ecs])
 
     return ecs.get(archetype)
