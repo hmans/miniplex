3a0b5e479ee7048947c992fd4b7a2469fd8270ae
Hendrik Mans <hendrik@mans.de>
2022-02-10 19:51:36 +0100

    Squashed commit of the following:
    commit 8d8f31c2475428786ea21842865be26c9948a296
    Author: Hendrik Mans <hendrik@mans.de>
    Date:   Thu Feb 10 19:50:39 2022 +0100
    
        Fix config
    
    commit 2eb07d8c7d156160ab65aed97d9753f53a8f075d
    Merge: b047d20 f8ff3c2
    Author: Hendrik Mans <hendrik@mans.de>
    Date:   Thu Feb 10 19:41:11 2022 +0100
    
        Merge branch 'main' into rollup-entrypoints
    
        * main:
          Fix type issues in react.ts
    
    commit b047d2051ee4772fd3b8786e0985ec4d81376777
    Author: Hendrik Mans <hendrik@mans.de>
    Date:   Thu Feb 10 19:39:08 2022 +0100
    
        Set up

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import { terser } from "rollup-plugin-terser"
 import typescript from "rollup-plugin-typescript2"
+import multiEntry from "rollup-plugin-multi-entry"
 import pkg from "./package.json"
 
 const defaults = {
-  input: "src/index.ts",
-  external: ["react"],
+  external: (name) => name =~ /^react\//,
   plugins: [
+    multiEntry(),
     typescript({
       typescript: require("typescript")
     }),
@@ -16,6 +17,7 @@ const defaults = {
 export default [
   {
     ...defaults,
+    input: "src/index.ts",
     output: {
       file: pkg.main,
       format: "cjs",
@@ -24,8 +26,27 @@ export default [
   },
   {
     ...defaults,
+    input: "src/index.ts",
     output: {
       file: pkg.module,
+      format: "esm",
+      sourcemap: true
+    }
+  },
+  {
+    ...defaults,
+    input: "src/react.tsx",
+    output: {
+      file: "dist/react.js",
+      format: "cjs",
+      sourcemap: true
+    }
+  },
+  {
+    ...defaults,
+    input: "src/react.tsx",
+    output: {
+      file: "dist/react.esm.js",
       format: "esm",
       sourcemap: true
     }
