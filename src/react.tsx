3c3038cb9f69a4f5e74dfc3e0978b1020b008385
Hendrik Mans <hendrik@mans.de>
2022-02-15 05:44:07 +0100

    Add some comments to the react integration

--- src/react.tsx
+++ src/react.tsx
@@ -10,37 +10,37 @@ import { useRerender } from "./util/useRerender"
  * @param world An instance of a Miniplex ECS to use.
  */
 export function createReactIntegration<T extends IEntity = UntypedEntity>(world: World<T>) {
-  function useArchetype(...names: ComponentName<T>[]) {
-    const rerender = useRerender()
-    const archetype = world.createArchetype(...names)
-
-    useEffect(() => {
-      world.listeners.archetypeChanged.get(archetype)!.on(rerender)
-      return () => world.listeners.archetypeChanged.get(archetype)!.off(rerender)
-    }, [world])
-
-    return world.get(archetype)
-  }
-
   const EntityContext = createContext<T>(null!)
 
+  /**
+   * A React component to either create a new entity, or represent an existing entity so
+   * it can be enhanced with additional components (see the <Component> component.)
+   */
   const Entity: FC<{ entity?: T }> = ({ entity: existingEntity, children }) => {
+    /* Reuse the specified entity or create a new one */
     const [entity] = useState<T>(() => existingEntity ?? ({} as T))
 
+    /* If the entity was freshly created, manage its presence in the ECS world. */
     useEffect(() => {
       if (existingEntity) return
-
       world.immediately.addEntity(entity)
       return () => world.immediately.removeEntity(entity)
     }, [entity])
 
+    /* Provide a context with the entity so <Component> components can be wired up. */
     return <EntityContext.Provider value={entity}>{children}</EntityContext.Provider>
   }
 
+  /**
+   * Return the current entity context.
+   */
   function useEntity() {
     return useContext(EntityContext)
   }
 
+  /**
+   * Declarative declare a component on an entity.
+   */
   function Component<K extends keyof T>({ name, data }: { name: K; data: T[K] }) {
     const entity = useEntity()
 
@@ -50,6 +50,22 @@ export function createReactIntegration<T extends IEntity = UntypedEntity>(world:
     }, [entity, name, data])
 
     return null
+  }
+
+  /**
+   * Return the entities of the specified archetype and subscribe this component
+   * to it, making it re-render when entities are added to or removed from it.
+   */
+  function useArchetype(...names: ComponentName<T>[]) {
+    const rerender = useRerender()
+    const archetype = world.createArchetype(...names)
+
+    useEffect(() => {
+      world.listeners.archetypeChanged.get(archetype)!.on(rerender)
+      return () => world.listeners.archetypeChanged.get(archetype)!.off(rerender)
+    }, [world, archetype])
+
+    return world.get(archetype)
   }
 
   return { useArchetype, useEntity, Entity, Component }
