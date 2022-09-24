7dce11a7ae62f90a7d003601c553c1238b75e6d0
Hendrik Mans <hendrik@mans.de>
2022-09-24 13:56:29 +0200

    Remove unregisterEntity

--- packages/miniplex/src/World.ts
+++ packages/miniplex/src/World.ts
@@ -100,11 +100,6 @@ export class World<T extends IEntity = UntypedEntity> {
 
   /* MUTATION FUNCTIONS */
 
-  private unregisterEntity = (entity: RegisteredEntity<T>) => {
-    delete (entity as T).__miniplex
-    return entity as T
-  }
-
   public createEntity<I extends T>(entity: I): RegisteredEntity<T> & I {
     /* Mix in internal component into entity. */
     const registeredEntity = Object.assign(entity, {
@@ -136,7 +131,7 @@ export class World<T extends IEntity = UntypedEntity> {
     }
 
     /* Remove its miniplex component */
-    this.unregisterEntity(entity)
+    delete (entity as T).__miniplex
   }
 
   public addComponent = (
