fe8e342b4b537ebf0b07e4fa52eb6e2c0d415413
Hendrik Mans <hendrik@mans.de>
2022-10-10 14:10:40 +0200

    Tweak main

--- apps/strict-mode-test/src/main.tsx
+++ apps/strict-mode-test/src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
+const STRICT = true
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  STRICT ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
     <App />
-  </React.StrictMode>
+  )
 )
