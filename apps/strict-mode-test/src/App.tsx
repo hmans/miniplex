5f050437eed29132734a05a3d898f9014e14caeb
Hendrik Mans <hendrik@mans.de>
2022-10-10 12:29:13 +0200

    Make test app a little smarter

--- apps/strict-mode-test/src/App.tsx
+++ apps/strict-mode-test/src/App.tsx
@@ -2,22 +2,27 @@ import { createECS } from "miniplex-react"
 import { useEffect, useState } from "react"
 import "./App.css"
 
-const ECS = createECS<{ count: number }>()
+const ECS = createECS<{ count: number; div: HTMLDivElement }>()
 
 function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    console.log("Count on the component is:", ECS.world.entities[0]?.count)
+    console.log(ECS.world.entities.length)
+    console.log(ECS.world.entities[0])
+    console.log("Count on the component is:", ECS.world.entities[0].count)
+    console.log("DIV on the component is:", ECS.world.entities[0].div)
   })
 
   return (
     <div>
       <ECS.Entity>
         <ECS.Component name="count" data={count} />
+        <ECS.Component name="div">
+          <div>I'm a DIV, my count is {count}!</div>
+        </ECS.Component>
       </ECS.Entity>
 
-      <p>Count is: {count}</p>
       <button onClick={() => setCount((c) => c + 1)}>
         Increase & Rerender
       </button>
