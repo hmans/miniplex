8cff9501d832111584ddb6b8e819d6f18a789dd2
Hendrik Mans <hendrik@mans.de>
2022-10-13 13:18:42 +0200

    Make gravity snappier

--- apps/demo/src/systems/physicsSystem.ts
+++ apps/demo/src/systems/physicsSystem.ts
@@ -1,11 +1,14 @@
+import { Vector3 } from "three"
 import { BOUNDS, ECS } from "../state"
 
 const entities = ECS.world.archetype("transform", "physics")
 
+const gravity = new Vector3(0, -20, 0)
+
 export function physicsSystem(dt: number) {
   for (const { transform, physics } of entities) {
     /* Apply gravity */
-    physics.velocity.y -= 9.8 * dt
+    physics.velocity.addScaledVector(gravity, dt)
 
     /* Apply velocity */
     transform.position.addScaledVector(physics.velocity, dt)
