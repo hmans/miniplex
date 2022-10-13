e74d73dec4894aa1c9b8f4cd0fbb19c157408b5a
Hendrik Mans <hendrik@mans.de>
2022-10-13 13:08:46 +0200

    Box

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,5 +1,7 @@
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { useEffect } from "react"
+import { DoubleSide } from "three"
 import { Balls, spawnBall } from "./Balls"
 import { Systems } from "./Systems"
 
@@ -17,9 +19,20 @@ function App() {
   return (
     <Canvas>
       <ambientLight />
-      <directionalLight position={[10, 10, 5]} />
+      <directionalLight position={[30, 20, 10]} />
+      <PerspectiveCamera position={[0, 0, 30]} makeDefault />
+      <OrbitControls />
 
-      <Balls />
+      <mesh>
+        <boxGeometry args={[10, 10, 10]} />
+        <meshPhysicalMaterial
+          color="#444"
+          transparent
+          opacity={0.1}
+          side={DoubleSide}
+        />
+        <Balls />
+      </mesh>
 
       <Systems />
     </Canvas>
