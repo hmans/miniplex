7afc2b56453d83cc7e5da6a846cb380d36e00d54
Hendrik Mans <hendrik@mans.de>
2022-10-13 20:25:33 +0200

    Suspense

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,7 +1,7 @@
 import { Environment, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Perf } from "r3f-perf"
-import { StrictMode } from "react"
+import { StrictMode, Suspense } from "react"
 import { Balls } from "./Balls"
 import { Box } from "./Box"
 import { Systems } from "./Systems"
@@ -11,29 +11,31 @@ function App() {
     <Canvas shadows>
       <StrictMode>
         <color args={["#457b9d"]} attach="background" />
-        <Environment preset="sunset" />
-        <ambientLight intensity={0.2} />
-        <directionalLight
-          position={[10, 10, 30]}
-          castShadow
-          intensity={1}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={200}
-          shadow-camera-left={-100}
-          shadow-camera-right={100}
-          shadow-camera-top={100}
-          shadow-camera-bottom={-100}
-        />
+        <Suspense>
+          <Environment preset="sunset" />
+          <ambientLight intensity={0.2} />
+          <directionalLight
+            position={[10, 10, 30]}
+            castShadow
+            intensity={1}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={200}
+            shadow-camera-left={-100}
+            shadow-camera-right={100}
+            shadow-camera-top={100}
+            shadow-camera-bottom={-100}
+          />
 
-        <PerspectiveCamera position={[0, 0, 30]} makeDefault />
+          <PerspectiveCamera position={[0, 0, 30]} makeDefault />
 
-        <Box>
-          <Balls />
-        </Box>
+          <Box>
+            <Balls />
+          </Box>
 
-        <Systems />
-        <Perf position="bottom-right" matrixUpdate />
+          <Systems />
+          <Perf position="bottom-right" matrixUpdate />
+        </Suspense>
       </StrictMode>
     </Canvas>
   )
