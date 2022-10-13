f6ee336fd6fffb243ef918a76dc0adeb113e939f
Hendrik Mans <hendrik@mans.de>
2022-10-13 13:56:12 +0200

    Cleanup

--- apps/demo/src/Balls.tsx
+++ apps/demo/src/Balls.tsx
@@ -4,18 +4,14 @@ import { Vector3 } from "three"
 import { InstancedParticles, Particle } from "vfx-composer-r3f"
 import { ECS } from "./state"
 
-export const Balls = () => {
-  return (
-    <InstancedParticles castShadow receiveShadow>
-      <sphereGeometry args={[1, 12, 12]} />
-      <meshStandardMaterial color="#f1faee" />
+export const Balls = () => (
+  <InstancedParticles castShadow receiveShadow>
+    <sphereGeometry args={[1, 12, 12]} />
+    <meshStandardMaterial color="#f1faee" />
 
-      <ECS.Archetype properties="isBall">
-        {(entity) => entity.jsx}
-      </ECS.Archetype>
-    </InstancedParticles>
-  )
-}
+    <ECS.Archetype properties="isBall">{(entity) => entity.jsx}</ECS.Archetype>
+  </InstancedParticles>
+)
 
 export const spawnBall = (props: GroupProps) => {
   const radius = between(0.1, 0.3)
