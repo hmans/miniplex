15a8c462e95e6a515ef96affa6e560b4194e53fa
Hendrik Mans <hendrik@mans.de>
2022-10-13 13:22:46 +0200

    Add physics

--- apps/demo/src/systems/physicsSystem.ts
+++ apps/demo/src/systems/physicsSystem.ts
@@ -1,12 +1,17 @@
+import { EntityWith } from "miniplex"
 import { Vector3 } from "three"
-import { BOUNDS, ECS } from "../state"
+import { BOUNDS, ECS, Entity } from "../state"
 
-const entities = ECS.world.archetype("transform", "physics")
+const { entities } = ECS.world.archetype("transform", "physics")
 
 const gravity = new Vector3(0, -20, 0)
 
+const tmpVec3 = new Vector3()
+
 export function physicsSystem(dt: number) {
-  for (const { transform, physics } of entities) {
+  for (let i = 0; i < entities.length; i++) {
+    const { transform, physics } = entities[i]
+
     /* Apply gravity */
     physics.velocity.addScaledVector(gravity, dt)
 
@@ -18,5 +23,40 @@ export function physicsSystem(dt: number) {
       transform.position.y = -BOUNDS
       physics.velocity.y *= -physics.restitution
     }
+
+    /* Ball collisions */
+    for (let j = i + 1; j < entities.length; j++) {
+      handleBallCollision(entities[i], entities[j])
+    }
   }
+}
+
+function handleBallCollision(
+  a: EntityWith<Entity, "transform" | "physics">,
+  b: EntityWith<Entity, "transform" | "physics">
+) {
+  const diff = tmpVec3.subVectors(a.transform.position, b.transform.position)
+  const distance = diff.length()
+  const penetration = (a.physics.radius + b.physics.radius - distance) / 2.0
+
+  if (penetration <= 0) return
+
+  const normal = diff.normalize()
+
+  /* Shift objects */
+  a.transform.position.addScaledVector(normal, penetration)
+  b.transform.position.addScaledVector(normal, -penetration)
+
+  /* Adjust velocities */
+  const aVel = a.physics.velocity.dot(normal)
+  const bVel = b.physics.velocity.dot(normal)
+
+  const aMass = a.physics.mass
+  const bMass = b.physics.mass
+
+  const aNewVel = (aVel * (aMass - bMass) + 2 * bMass * bVel) / (aMass + bMass)
+  const bNewVel = (bVel * (bMass - aMass) + 2 * aMass * aVel) / (aMass + bMass)
+
+  a.physics.velocity.addScaledVector(normal, aNewVel - aVel)
+  b.physics.velocity.addScaledVector(normal, bNewVel - bVel)
 }
