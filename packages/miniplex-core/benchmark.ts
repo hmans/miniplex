90ad30af34dbce884f8f49f7197704188261087e
Hendrik Mans <hendrik@mans.de>
2022-10-15 08:00:58 +0200

    Benchmark creation

--- packages/miniplex-core/benchmark.ts
+++ packages/miniplex-core/benchmark.ts
@@ -1,3 +1,29 @@
 import { World } from "./src"
 
-console.log("hello")
+const count = 100_000
+
+const profile = <T>(name: string, fn: () => T) => {
+  const before = Date.now()
+  const result = fn()
+  const after = Date.now()
+  console.log(`${name} took ${after - before}ms`)
+  return result
+}
+
+type Entity = {
+  position: {
+    x: number
+    y: number
+    z: number
+  }
+  age?: number
+}
+
+const world = new World<Entity>()
+
+const createEntities = () => {
+  for (let i = 0; i < count; i++)
+    world.add({ position: { x: 0, y: i, z: 0 }, age: i })
+}
+
+profile("create", createEntities)
