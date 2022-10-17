1b8b78afe2601efd3def9b022dafe21ca7b6d299
Hendrik Mans <hendrik@mans.de>
2022-10-17 16:44:22 +0200

    Add a simple scene

--- apps/vanilla-demo/src/main.ts
+++ apps/vanilla-demo/src/main.ts
@@ -1,1 +1,42 @@
 import "./style.css"
+import * as THREE from "three"
+
+function engineSystem() {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+
+  camera.position.z = 30
+  scene.add(camera)
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  const mesh = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(),
+    new THREE.MeshBasicMaterial({ color: "red" })
+  )
+
+  scene.add(mesh)
+
+  return () => {
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01
+
+    renderer.render(scene, camera)
+  }
+}
+
+const update = engineSystem()
+
+function tick() {
+  update()
+  requestAnimationFrame(tick)
+}
+
+tick()
