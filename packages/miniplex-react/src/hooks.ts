9b13e9566c60c59b6229acf0ce5892033577e263
Hendrik Mans <hendrik@mans.de>
2022-10-19 16:15:21 +0200

    Fix

--- packages/miniplex-react/src/hooks.ts
+++ packages/miniplex-react/src/hooks.ts
@@ -1,5 +1,5 @@
 import { useRerender } from "@hmans/use-rerender"
-import { Bucket, IEntity, World } from "@miniplex/core"
+import { all, Bucket, IEntity, World } from "@miniplex/core"
 import useIsomorphicLayoutEffect from "./isomorphicLayoutEffect"
 
 export const useEntities = <E extends IEntity>(bucket: Bucket<E>) => {
@@ -23,4 +23,4 @@ export const useEntities = <E extends IEntity>(bucket: Bucket<E>) => {
 export const useArchetype = <E extends IEntity, P extends keyof E>(
   world: World<E>,
   ...properties: P[]
-) => useEntities(world.archetype(...properties))
+) => useEntities(world.derive(all(...properties)))
