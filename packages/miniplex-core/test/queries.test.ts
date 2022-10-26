ec9b3f30253bd3a3ebe2eb3e2f6593c64a053224
Hendrik Mans <hendrik@mans.de>
2022-10-26 12:31:58 +0200

    matchy

--- packages/miniplex-core/test/queries.test.ts
+++ packages/miniplex-core/test/queries.test.ts
@@ -1,6 +1,6 @@
-import { matches } from "../src/queries"
+import { componentsMatch, entityMatches } from "../src/queries"
 
-describe("matches", () => {
+describe("entityMatches", () => {
   type Entity = {
     name: string
     age?: number
@@ -10,26 +10,66 @@ describe("matches", () => {
   const entity: Entity = { name: "John", age: 42 }
 
   it("returns true if 'all' components are present", () => {
-    expect(matches(entity, { all: ["name", "age"] })).toBe(true)
+    expect(entityMatches(entity, { all: ["name", "age"] })).toBe(true)
   })
 
   it("returns false if any 'all' component is missing", () => {
-    expect(matches(entity, { all: ["name", "age", "height"] })).toBe(false)
+    expect(entityMatches(entity, { all: ["name", "age", "height"] })).toBe(
+      false
+    )
   })
 
   it("returns true if any 'any' component is present", () => {
-    expect(matches(entity, { any: ["name", "height"] })).toBe(true)
+    expect(entityMatches(entity, { any: ["name", "height"] })).toBe(true)
   })
 
   it("returns false if all 'any' components are missing", () => {
-    expect(matches(entity, { any: ["height"] })).toBe(false)
+    expect(entityMatches(entity, { any: ["height"] })).toBe(false)
   })
 
   it("returns true if no 'none' components are present", () => {
-    expect(matches(entity, { none: ["height"] })).toBe(true)
+    expect(entityMatches(entity, { none: ["height"] })).toBe(true)
   })
 
   it("returns false if any 'none' component is present", () => {
-    expect(matches(entity, { none: ["name", "height"] })).toBe(false)
+    expect(entityMatches(entity, { none: ["name", "height"] })).toBe(false)
+  })
+})
+
+describe("componentsMatch", () => {
+  type Entity = {
+    name: string
+    age?: number
+    height?: number
+  }
+
+  const components: (keyof Entity)[] = ["name", "age"]
+
+  it("returns true if 'all' components are present", () => {
+    expect(componentsMatch(components, { all: ["name", "age"] })).toBe(true)
+  })
+
+  it("returns false if any 'all' component is missing", () => {
+    expect(
+      componentsMatch(components, { all: ["name", "age", "height"] })
+    ).toBe(false)
+  })
+
+  it("returns true if any 'any' component is present", () => {
+    expect(componentsMatch(components, { any: ["name", "height"] })).toBe(true)
+  })
+
+  it("returns false if all 'any' components are missing", () => {
+    expect(componentsMatch(components, { any: ["height"] })).toBe(false)
+  })
+
+  it("returns true if no 'none' components are present", () => {
+    expect(componentsMatch(components, { none: ["height"] })).toBe(true)
+  })
+
+  it("returns false if any 'none' component is present", () => {
+    expect(componentsMatch(components, { none: ["name", "height"] })).toBe(
+      false
+    )
   })
 })
