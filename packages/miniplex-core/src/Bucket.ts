d13f17cebc5f0e4fd815bb442f10d8cb2e27a0aa
Hendrik Mans <hendrik@mans.de>
2022-10-26 12:07:10 +0200

    Yo

--- packages/miniplex-core/src/Bucket.ts
+++ packages/miniplex-core/src/Bucket.ts
@@ -10,4 +10,17 @@ export class Bucket<E extends IEntity> {
   constructor(opts: BucketOptions<E> = {}) {
     this.entities = opts.entities || []
   }
+
+  add(entity: E) {
+    this.entities.push(entity)
+  }
+
+  remove(entity: E) {
+    const index = this.entities.indexOf(entity)
+
+    if (index !== -1) {
+      this.entities[index] = this.entities[this.entities.length - 1]
+      this.entities.pop()
+    }
+  }
 }
--- packages/miniplex-core/src/World.ts
+++ packages/miniplex-core/src/World.ts
@@ -6,19 +6,6 @@ export type WorldOptions<E extends IEntity> = {
 }
 
 export class World<E extends IEntity> extends Bucket<E> {
-  add(entity: E) {
-    this.entities.push(entity)
-  }
-
-  remove(entity: E) {
-    const index = this.entities.indexOf(entity)
-
-    if (index !== -1) {
-      this.entities[index] = this.entities[this.entities.length - 1]
-      this.entities.pop()
-    }
-  }
-
   addComponent<C extends keyof E>(entity: E, component: C, value: E[C]) {
     entity[component] = value
   }
