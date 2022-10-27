e55f33d3fef4cd5dc5cdb0c77cd8040e686be896
Hendrik Mans <hendrik@mans.de>
2022-10-27 04:41:37 +0200

    Remove querybuilder

--- packages/miniplex-core/src/queries.ts
+++ packages/miniplex-core/src/queries.ts
@@ -12,31 +12,3 @@ export const normalizeQuery = <E extends IEntity>(query: Query<E>) => ({
 
 export const serializeQuery = <E extends IEntity>(query: Query<E>) =>
   JSON.stringify(query)
-
-class QueryBuilder<E extends IEntity> {
-  query: Query<E> = {}
-
-  all(...components: (keyof E)[]) {
-    this.query.all = components
-    return this
-  }
-
-  any(...components: (keyof E)[]) {
-    this.query.any = components
-    return this
-  }
-
-  none(...components: (keyof E)[]) {
-    this.query.none = components
-    return this
-  }
-}
-
-export const all = <E extends IEntity>(...components: (keyof E)[]) =>
-  new QueryBuilder<E>().all(...components)
-
-export const any = <E extends IEntity>(...components: (keyof E)[]) =>
-  new QueryBuilder<E>().any(...components)
-
-export const none = <E extends IEntity>(...components: (keyof E)[]) =>
-  new QueryBuilder<E>().none(...components)
