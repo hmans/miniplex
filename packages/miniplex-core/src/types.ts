cdcb8bb11cd6bba82e5acb2bb0f3261af00ac3c4
Hendrik Mans <hendrik@mans.de>
2022-10-27 12:03:21 +0200

    IQuery

--- packages/miniplex-core/src/types.ts
+++ packages/miniplex-core/src/types.ts
@@ -9,3 +9,9 @@ export type WithComponents<E, P extends keyof E> = E & {
 export type Predicate<E, D extends E = E> =
   | ((entity: any) => entity is D)
   | ((entity: any) => boolean)
+
+export interface IQuery<E extends IEntity, All extends keyof E = keyof E> {
+  all?: All[]
+  any?: (keyof E)[]
+  none?: (keyof E)[]
+}
