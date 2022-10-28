2d928b7591240e23de46a83085de19437f31d7f3
Hendrik Mans <hendrik@mans.de>
2022-10-28 11:10:43 +0200

    addComponent

--- packages/miniplex-core/src/World.ts
+++ packages/miniplex-core/src/World.ts
@@ -13,6 +13,26 @@ export class World<E extends IEntity> extends Bucket<E> {
     }
   }
 
+  addComponent<C extends keyof E>(entity: E, component: C, value: E[C]) {
+    /* Return early if the entity already has the component. */
+    if (entity[component] !== undefined) return
+
+    /* Set the component */
+    entity[component] = value
+
+    /* If this world doesn't know about the entity, we're done. */
+    if (!this.has(entity)) return
+
+    /* Update archetypes */
+    for (const [predicate, archetype] of this.archetypes) {
+      if (predicate(entity)) {
+        archetype.add(entity)
+      } else {
+        archetype.remove(entity)
+      }
+    }
+  }
+
   private archetypes = new Map<Predicate<E>, Bucket<any>>()
 
   archetype<D extends E>(predicate: Predicate<D>) {
--- packages/miniplex-core/test/World.test.ts
+++ packages/miniplex-core/test/World.test.ts
@@ -30,4 +30,34 @@ describe(World, () => {
       expect(world.archetype(hasAge)).toBe(archetype)
     })
   })
+
+  describe("addComponent", () => {
+    it("adds the component to the entity", () => {
+      const world = new World<Entity>()
+      const john = world.add({ name: "John" })
+
+      world.addComponent(john, "age", 123)
+
+      expect(john.age).toBe(123)
+    })
+
+    it("does nothing if the component already exists on the entity", () => {
+      const world = new World<Entity>()
+      const john = world.add({ name: "John", age: 123 })
+
+      world.addComponent(john, "age", 456)
+
+      expect(john.age).toBe(123)
+    })
+
+    it("adds the entity to relevant archetypes", () => {
+      const world = new World<Entity>()
+      const john = world.add({ name: "John" })
+      const archetype = world.archetype(hasAge)
+
+      world.addComponent(john, "age", 123)
+
+      expect(archetype.has(john)).toBe(true)
+    })
+  })
 })
