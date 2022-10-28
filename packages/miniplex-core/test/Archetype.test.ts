de132d60ef311cbb49e57009cc6cbe02702ba23e
Hendrik Mans <hendrik@mans.de>
2022-10-28 13:55:47 +0200

    More tests

--- packages/miniplex-core/test/Archetype.test.ts
+++ packages/miniplex-core/test/Archetype.test.ts
@@ -7,12 +7,35 @@ type Entity = {
 }
 
 describe(Archetype, () => {
-  it("creates an archetype index containg all entities that have a specific set of components", () => {
+  it("creates an archetype bucket containg all entities that have a specific set of components", () => {
     const world = new World<Entity>()
-    const archetype = new Archetype(world, { all: ["age"] })
+    const archetype = new Archetype(world, { all: ["age", "name"] })
 
-    world.add({ name: "John" })
-    world.add({ name: "Jane", age: 123 })
-    expect(archetype.entities).toEqual([{ name: "Jane", age: 123 }])
+    const john = world.add({ name: "John" })
+    const jane = world.add({ name: "Jane", age: 123 })
+
+    expect(archetype.entities).toEqual([jane])
+  })
+
+  it("creates an archetype bucket containing all entities that have at least one of the specified components", () => {
+    const world = new World<Entity>()
+    const archetype = new Archetype(world, { any: ["age", "height"] })
+
+    const john = world.add({ name: "John" })
+    const jane = world.add({ name: "Jane", age: 123 })
+    const jack = world.add({ name: "Jack", height: 123 })
+
+    expect(archetype.entities).toEqual([jane, jack])
+  })
+
+  it("creates an archetype bucket containing all entities that have none of the specified components", () => {
+    const world = new World<Entity>()
+    const archetype = new Archetype(world, { none: ["age", "height"] })
+
+    const john = world.add({ name: "John" })
+    const jane = world.add({ name: "Jane", age: 123 })
+    const jack = world.add({ name: "Jack", height: 123 })
+
+    expect(archetype.entities).toEqual([john])
   })
 })
