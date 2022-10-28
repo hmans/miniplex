81497041a35a15f9adf181dd0927c8ca8301b9a3
Hendrik Mans <hendrik@mans.de>
2022-10-28 18:17:28 +0200

    entitypositions optimization

--- packages/miniplex-bucket/src/Bucket.ts
+++ packages/miniplex-bucket/src/Bucket.ts
@@ -17,17 +17,20 @@ export class Bucket<E> {
   onEntityAdded = new Event<E>()
   onEntityRemoved = new Event<E>()
 
+  private entityPositions = new Map<E, number>()
+
   get size() {
     return this.entities.length
   }
 
   has(entity: any): entity is E {
-    return this.entities.includes(entity)
+    return this.entityPositions.has(entity)
   }
 
   add<D extends E>(entity: D): D & E {
     if (entity && !this.has(entity)) {
       this.entities.push(entity)
+      this.entityPositions.set(entity, this.entities.length - 1)
       this.onEntityAdded.emit(entity)
     }
 
@@ -36,8 +39,9 @@ export class Bucket<E> {
 
   remove(entity: E) {
     if (this.has(entity)) {
-      const index = this.entities.indexOf(entity)
+      const index = this.entityPositions.get(entity)!
       this.entities.splice(index, 1)
+      this.entityPositions.delete(entity)
       this.onEntityRemoved.emit(entity)
     }
 
