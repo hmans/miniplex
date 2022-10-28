9c66790c58f73f2e224c369a1e8da90bd8deb025
Hendrik Mans <hendrik@mans.de>
2022-10-28 17:41:06 +0200

    Tests for queries

---
+++ packages/miniplex-core/test/queries.test.ts
@@ -0,0 +1,37 @@
+import { has, hasNone, hasSome, not } from "../src/queries"
+
+describe(has, () => {
+  it("returns true if the entity has all of the specified components", () => {
+    expect(has("name", "age")({ name: "John", age: 123 })).toBe(true)
+  })
+
+  it("returns false if the entity does not have all of the specified components", () => {
+    expect(has("name", "age")({ name: "John" })).toBe(false)
+  })
+})
+
+describe(hasSome, () => {
+  it("returns true if the entity has at least one of the specified components", () => {
+    expect(hasSome("name", "age")({ name: "John" } as any)).toBe(true)
+  })
+
+  it("returns false if the entity has none of the specified components", () => {
+    expect(hasSome("name", "age")({} as any)).toBe(false)
+  })
+})
+
+describe(hasNone, () => {
+  it("returns true if the entity has none of the specified components", () => {
+    expect(hasNone("name", "age")({} as any)).toBe(true)
+  })
+
+  it("returns false if the entity has at least one of the specified components", () => {
+    expect(hasNone("name", "age")({ name: "John" } as any)).toBe(false)
+  })
+})
+
+describe(not, () => {
+  it("negates the predicte", () => {
+    expect(not(has("name", "age"))({ name: "John" } as any)).toBe(true)
+  })
+})
