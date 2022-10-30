2e49d7083da580210add5e13bb98ebc2f984d972
Hendrik Mans <hendrik@mans.de>
2022-10-30 23:40:54 +0100

    Remove some uses of WithComponent

--- apps/demo/src/entities/Asteroids.tsx
+++ apps/demo/src/entities/Asteroids.tsx
@@ -1,5 +1,5 @@
 import { Composable, Modules } from "material-composer-r3f"
-import { archetype, Predicate, WithComponents } from "miniplex"
+import { archetype, Predicate } from "miniplex"
 import { insideCircle, power } from "randomish"
 import { useLayoutEffect } from "react"
 import { $, Input, InstanceID, Lerp } from "shader-composer"
@@ -11,7 +11,7 @@ import { ECS, Entity, physics, PhysicsLayers } from "../state"
 import { bitmask } from "../util/bitmask"
 import { RenderableEntity } from "./RenderableEntity"
 
-export type Asteroid = WithComponents<
+export type Asteroid = Pick<
   Entity,
   | "isAsteroid"
   | "transform"
--- apps/demo/src/entities/RenderableEntity.tsx
+++ apps/demo/src/entities/RenderableEntity.tsx
@@ -1,8 +1,7 @@
-import { WithComponents } from "miniplex"
 import { Entity } from "../state"
 
 export const RenderableEntity = ({
   entity
 }: {
-  entity: WithComponents<Entity, "render">
+  entity: Pick<Entity, "render">
 }) => <>{entity.render}</>
