2f9613984cecfaf62fec27911fa9fc86d9e7e176
Hendrik Mans <hendrik@mans.de>
2022-10-31 12:47:44 +0100

    Remove WithOnly

--- packages/miniplex-core/src/types.ts
+++ packages/miniplex-core/src/types.ts
@@ -4,12 +4,6 @@
 export type With<E, P extends keyof E> = E & Required<Pick<E, P>>
 
 /**
- * A utility type that marks the specified properties as required,
- * and removes all other properties.
- */
-export type WithOnly<E, P extends keyof E> = Strictly<With<E, P>>
-
-/**
  * A utility type that removes all optional properties.
  */
 export type Strictly<T> = WithoutOptional<T>
