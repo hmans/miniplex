623d227fd03f2ef0dc772b4d8b52f93aafe53fae
Hendrik Mans <hendrik@mans.de>
2022-10-31 12:18:08 +0100

    Codedocs

--- packages/miniplex-core/src/types.ts
+++ packages/miniplex-core/src/types.ts
@@ -1,3 +1,10 @@
+/**
+ * A utility type that marks the specified properties as required.
+ */
 export type With<E, P extends keyof E> = E & WithOnly<E, P>
 
+/**
+ * A utility type that marks the specified properties as required,
+ * and removes all other properties.
+ */
 export type WithOnly<E, P extends keyof E> = Required<Pick<E, P>>
