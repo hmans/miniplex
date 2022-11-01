e9cab16a86b48ae28284a21dc7eae26d4872a2bb
Hendrik Mans <hendrik@mans.de>
2022-11-01 16:19:43 +0100

    Re-add helpers to queries

--- packages/miniplex-core/src/queries.ts
+++ packages/miniplex-core/src/queries.ts
@@ -1,4 +1,4 @@
-import { ArchetypeQuery, Predicate } from "./types"
+import { ArchetypeQuery, Predicate, With } from "./types"
 import { Memoizer } from "./util/Memoizer"
 
 export const normalizeComponents = (components: any[]) => [
@@ -27,4 +27,32 @@ export const memoizeQuery = <Q extends ArchetypeQuery<any, any>>(query: Q) => {
   const normalizedQuery = normalizeQuery(query)
   const key = JSON.stringify(normalizedQuery)
   return queryCache.get(key, normalizedQuery) as Q
+}
+
+export function isArchetype<E, P extends keyof E>(
+  entity: E,
+  ...components: P[]
+): entity is With<E, P> {
+  return hasComponents(entity, ...components)
+}
+
+export function hasComponents<E, C extends keyof E>(
+  entity: E,
+  ...components: C[]
+): entity is With<E, C> {
+  return components.every((c) => entity[c] !== undefined)
+}
+
+export function hasAnyComponents<E, C extends keyof E>(
+  entity: E,
+  ...components: C[]
+): entity is With<E, C> {
+  return components.some((c) => entity[c] !== undefined)
+}
+
+export function hasNoComponents<E, C extends keyof E>(
+  entity: E,
+  ...components: C[]
+): entity is With<E, C> {
+  return components.every((c) => entity[c] === undefined)
 }
