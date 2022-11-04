3c01fe6bd699fdd22b413a254b1a91cbe9fab65c
Hendrik Mans <hendrik@mans.de>
2022-11-04 12:27:55 +0100

    FIGHT THE DEVIL

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -96,22 +96,14 @@ function Header() {
           <div>
             <a href="https://miniplex.hmans.co/" target="_blank">
               miniplex.hmans.co
-              <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                />
-              </svg>
             </a>
           </div>
-          <ul>
+          {/* <ul>
             <For
               each={socials}
               children={(social) => <SocialIcon {...social} />}
             />
-          </ul>
+          </ul> */}
         </div>
       </div>
     </header>
@@ -278,18 +270,6 @@ export default function Root() {
 
       <Body>
         <Header />
-
-        <input type="checkbox" name="sidebar-toggle" id="sidebar-toggle" />
-
-        <label for="sidebar-toggle">
-          <svg viewBox="0 0 24 24" style="fill: none; stroke: currentcolor;">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9 5l7 7-7 7"
-            ></path>
-          </svg>
-        </label>
 
         <Nav />
 
