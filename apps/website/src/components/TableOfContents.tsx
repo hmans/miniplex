5e14a68fb48c7d8ca3b08c375ccd8ebe215cb1e2
Hendrik Mans <hendrik@mans.de>
2022-11-05 09:11:09 +0100

    Exorcise Tailwind (#247)

--- apps/website/src/components/TableOfContents.tsx
+++ apps/website/src/components/TableOfContents.tsx
@@ -1,46 +1,41 @@
-import { For, Suspense } from "solid-js";
-import { A, useLocation } from "solid-start";
-import { createServerData$ } from "solid-start/server";
-import { mods } from "../root";
+import { For, Suspense } from "solid-js"
+import { A, useLocation } from "solid-start"
+import { createServerData$ } from "solid-start/server"
+import { mods } from "../root"
 
 export function useTableOfContents() {
-  const path = useLocation();
+  const path = useLocation()
   return createServerData$(
-    async pathname => {
-      let mod = mods[`./routes${pathname}.mdx`] ?? mods[`./routes${pathname}.md`];
-      if (!mod) return [];
-      return mod.getHeadings().filter(h => h.depth > 1 && h.depth <= 3);
+    async (pathname) => {
+      let mod =
+        mods[`./routes${pathname}.mdx`] ?? mods[`./routes${pathname}.md`]
+      if (!mod) return []
+      return mod.getHeadings().filter((h) => h.depth > 1 && h.depth <= 3)
     },
     {
       key: () => path.pathname
     }
-  );
+  )
 }
 
 export default function TableOfContents() {
-  const headings = useTableOfContents();
+  const headings = useTableOfContents()
+  const location = useLocation()
 
   return (
-    <div class="hidden col-start-3 row-start-2 xl:block px-8 py-8 space-y-4 w-[240px] overflow-hidden">
-      <span class="font-bold uppercase text-xs">On this page</span>
-      <ul class="space-y-4 text-sm">
+    <nav class="toc">
+      <h4>On this page:</h4>
+      <ul>
         <Suspense>
           <For each={headings()}>
-            {h => (
-              <li
-                classList={{
-                  "ml-2 font-semibold text-gray-500": h.depth === 2,
-                  "ml-4 text-gray-400": h.depth === 3
-                }}
-              >
-                <A activeClass="text-blue-400" href={`#${h.slug}`}>
-                  {h.text}
-                </A>
+            {(h) => (
+              <li>
+                <A href={`${location.pathname}#${h.slug}`}>{h.text}</A>
               </li>
             )}
           </For>
         </Suspense>
       </ul>
-    </div>
-  );
+    </nav>
+  )
 }
--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,19 +1,14 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
-import mdx from "solid-start-mdx";
+import solid from "solid-start/vite"
+import { defineConfig } from "vite"
+import mdx from "solid-start-mdx"
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [(await import("tailwindcss")).default]
-    }
-  },
   plugins: [
     await mdx(),
     solid({
       extensions: [".mdx", ".md"],
       islands: true,
       islandsRouter: true
-    }),
-  ],
-});
+    })
+  ]
+})
