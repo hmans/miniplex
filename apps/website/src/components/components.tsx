b1885487ff0d4106242641ff3331e9e7afc6cd89
Hendrik Mans <hendrik@mans.de>
2022-11-06 09:53:21 +0100

    Clean up components configuration

--- apps/website/src/components/components.tsx
+++ apps/website/src/components/components.tsx
@@ -8,55 +8,14 @@ export const components = {
       {props.children}
     </h1>
   ),
-  ssr: (props) => <>{props.children}</>,
-  spa: (props) => <></>,
-  p: (props) => <p {...props}>{props.children}</p>,
   a: (props) => {
     return <A {...props}>{props.children}</A>
   },
-  "token-link": (props) => {
-    return <span>{props.children}</span>
-  },
-  li: (props) => <li {...props}>{props.children}</li>,
-  ul: (props) => <ul {...props}>{props.children}</ul>,
-  ol: (props) => <ol {...props}>{props.children}</ol>,
-  nav: (props) => <nav {...props}>{props.children}</nav>,
   A,
-  "table-of-contents": (props) => {},
-  h5: (props) => <h5 {...props}>{props.children}</h5>,
-  "docs-error": (props) => {
-    return (
-      <div class="docs-error">
-        <p>
-          <span>Error:</span>
-          {props.children}
-        </p>
-      </div>
-    )
-  },
-  "docs-info": (props) => {
-    return (
-      <div class="docs-error">
-        <p>
-          <span>Error:</span>
-          {props.children}
-        </p>
-      </div>
-    )
-  },
   aside: (props) => (
     <aside {...props}>
       <div>WARNING</div>
       <div>{props.children}</div>
     </aside>
-  ),
-  response: (props) => {
-    return <span>{props.children}</span>
-  },
-  void: (props) => {
-    return <span>{props.children}</span>
-  },
-  unknown: (props) => {
-    return <span>{props.children}</span>
-  }
+  )
 }
