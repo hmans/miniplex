0edf78efda48a6e13a0f0d0f1f58068ce576a91f
Hendrik Mans <hendrik@mans.de>
2023-01-26 20:25:42 +0100

    Use it

--- apps/vanilla-demo/src/systems/transform.ts
+++ apps/vanilla-demo/src/systems/transform.ts
@@ -1,23 +1,29 @@
-import { World } from "miniplex"
+import { Monitor, World } from "miniplex"
 import { Entity } from "./engine"
 
 export function createTransformSystem(world: World<Entity>) {
   const entities = world.with("transform")
   const engines = world.with("engine")
 
-  entities.onEntityAdded.subscribe((entity) => {
-    const [{ engine }] = engines
+  const monitor = new Monitor(
+    entities,
 
-    if (entity.parent?.transform) {
-      entity.parent.transform.add(entity.transform)
-    } else {
-      engine.scene.add(entity.transform)
+    (entity) => {
+      const [{ engine }] = engines
+
+      if (entity.parent?.transform) {
+        entity.parent.transform.add(entity.transform)
+      } else {
+        engine.scene.add(entity.transform)
+      }
+    },
+
+    (entity) => {
+      entity.transform.parent?.remove(entity.transform)
     }
-  })
+  )
 
-  entities.onEntityRemoved.subscribe((entity) => {
-    entity.transform.parent?.remove(entity.transform)
-  })
-
-  return () => {}
+  return () => {
+    monitor.run()
+  }
 }
