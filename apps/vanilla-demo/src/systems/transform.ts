05c41bbe413d9528992976391b019bbbe3cb448f
Hendrik Mans <hendrik@mans.de>
2023-01-27 09:34:50 +0100

    explain

--- apps/vanilla-demo/src/systems/transform.ts
+++ apps/vanilla-demo/src/systems/transform.ts
@@ -7,18 +7,27 @@ export function createTransformSystem(world: World<Entity>) {
 
   const monitor = entities
     .monitor()
+
+    /* Mount a callback that will be executed for every entity arriving in this query */
     .setup(({ transform, parent }) => {
+      /* If the entity has a parent, add the transform to the parent's transform */
       if (parent?.transform) {
         parent.transform.add(transform)
-      } else {
-        const { engine } = engines.first!
-        engine.scene.add(transform)
+        return
       }
+
+      /* Otherwise, add the transform to the scene */
+      const { engine } = engines.first!
+      engine.scene.add(transform)
     })
+
+    /* Mount a callback that will be executed for every entity leaving this query */
     .teardown(({ transform }) => {
+      /* Remove the transform from its parent */
       transform.parent?.remove(transform)
     })
 
+  /* Return a function that will execute every frame */
   return () => {
     monitor.run()
   }
