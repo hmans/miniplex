b45eba0581e3c6fb6b32c6f6ff2aaf483e3645a2
Hendrik Mans <hendrik@mans.de>
2023-01-27 12:26:59 +0100

    re-export monitor from core

--- packages/miniplex-core/src/index.ts
+++ packages/miniplex-core/src/index.ts
@@ -1,4 +1,4 @@
-export { Bucket } from "@miniplex/bucket"
+export { Bucket, Monitor } from "@miniplex/bucket"
 export { Query, World } from "./core"
 export type { Strict, With, Without } from "./core"
 export { hasAnyComponents, hasComponents, hasNoComponents } from "./helpers"
