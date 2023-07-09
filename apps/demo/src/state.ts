b283d638413e70a230cee9ced61101d52f7fb7d8
Hendrik Mans <hendrik@mans.de>
2023-07-09 20:22:44 +0200

    Fix React demo, too

--- apps/demo/src/state.ts
+++ apps/demo/src/state.ts
@@ -1,7 +1,7 @@
-import { Object3D } from "three"
 import { World } from "miniplex"
-import createECS from "miniplex/react"
+import createECS from "miniplex-react"
 import { ReactNode } from "react"
+import { Object3D } from "three"
 
 export type Entity = {
   object3d?: Object3D
